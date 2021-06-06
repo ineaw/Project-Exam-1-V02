482dadbcd04ba462dc70dd8ea49aa51ae6393e17
ineaw <ineaw@users.noreply.github.com>
2021-06-06 10:31:11 +0200

    Changes after feedback

--- js/blog.js
+++ js/blog.js
@@ -1,14 +1,28 @@
-const url = "https://ineaw.no/the-green-side/wp-json/wp/v2/posts/";
+const url = "https://ineaw.no/the-green-side/wp-json/wp/v2/posts?page=";
 const postContainer = document.querySelector(".blog-grid");
 const morePosts = document.querySelector("#more");
 const loader = document.querySelector(".loader");
 const filterCategory = document.getElementById("filter-category");
 let optionsCat = "";
+let page = 1;
 
 async function getPosts() {
   try {
-    const response = await fetch(url + `?per_page=100` + `&_embed`);
+    const response = await fetch(url + page + `&_embed`);
     const result = await response.json();
+
+    // morePosts.innerText = page;
+    // if (page === 1) {
+    //   morePosts.disabeled = true;
+    // } else {
+    //   morePosts.disabeled = false;
+    // }
+    // if (result.length < 10) {
+    //   morePosts.disabeled = true;
+    // } else {
+    //   morePosts.disabeled = false;
+    // }
+
     createHTML(result);
   } catch (error) {
     console.log(error);
@@ -17,7 +31,7 @@ async function getPosts() {
 getPosts();
 
 function createHTML(result) {
-  for (let i = 0; i < 10; i++) {
+  for (let i = 0; i < result.length; i++) {
     {
       const img = result[i]._embedded["wp:featuredmedia"]["0"].source_url;
       const alt = result[i]._embedded["wp:featuredmedia"]["0"].alt_text;
@@ -44,42 +58,48 @@ function createHTML(result) {
 }
 
 morePosts.addEventListener("click", () => {
-  async function getPosts(url) {
-    try {
-      const response = await fetch(url + `?per_page=100` + `&_embed`);
-      const results = await response.json();
-      console.log(results);
-      for (let i = 10; i < results.length; i++) {
-        const img = results[i]._embedded["wp:featuredmedia"]["0"].source_url;
-        const alt = results[i]._embedded["wp:featuredmedia"]["0"].alt_text;
-        const postContent = results[i].excerpt.rendered;
-        const title = results[i].title.rendered;
-        console.log(results);
-        const blogPost = results[i].id;
-        const postDate = new Date(results[i].date).toLocaleString("en-US", {
-          month: "long",
-          day: "2-digit",
-        });
-        postContainer.innerHTML += `
-        <figure class="blog-post-card">
-         <a href="post.html?id=${blogPost}"> <img src="${img}" alt="${alt}"/></a>
-         <p class="blog-date"<time>${postDate}</time> | By <a href="about.html">Ine AW</a></p>
-         <h2 class="blog-title">${title}</h2>
-         <a href="post.html?id=${blogPost}" class="blog-link" aria-label="read more about ${title}">Read more</a>
-        </figure>`;
-        morePosts.style.display = "none";
-        loader.style.display = "none";
-      }
-    } catch (error) {
-      console.log(error);
-      postContainer.innerHTML = message("An error occured when trying to load", error);
-    }
-  }
-  getPosts(url);
+  page++;
+  getPosts();
+  morePosts.style.display = "none";
 });
 
-let page = 1;
-let totalpages = null;
+// morePosts.addEventListener("click", () => {
+//   async function getPosts(url) {
+//     try {
+//       const response = await fetch(url + `?per_page=100` + `&_embed`);
+//       const results = await response.json();
+//       console.log(results);
+//       for (let i = 10; i < results.length; i++) {
+//         const img = results[i]._embedded["wp:featuredmedia"]["0"].source_url;
+//         const alt = results[i]._embedded["wp:featuredmedia"]["0"].alt_text;
+//         const postContent = results[i].excerpt.rendered;
+//         const title = results[i].title.rendered;
+//         console.log(results);
+//         const blogPost = results[i].id;
+//         const postDate = new Date(results[i].date).toLocaleString("en-US", {
+//           month: "long",
+//           day: "2-digit",
+//         });
+//         postContainer.innerHTML += `
+//         <figure class="blog-post-card">
+//          <a href="post.html?id=${blogPost}"> <img src="${img}" alt="${alt}"/></a>
+//          <p class="blog-date"<time>${postDate}</time> | By <a href="about.html">Ine AW</a></p>
+//          <h2 class="blog-title">${title}</h2>
+//          <a href="post.html?id=${blogPost}" class="blog-link" aria-label="read more about ${title}">Read more</a>
+//         </figure>`;
+//         morePosts.style.display = "none";
+//         loader.style.display = "none";
+//       }
+//     } catch (error) {
+//       console.log(error);
+//       postContainer.innerHTML = message("An error occured when trying to load", error);
+//     }
+//   }
+//   getPosts(url);
+// });
+
+// let page = 1;
+// let totalpages = null;
 
 // filterCategory.addEventListener("change", (e) => {
 //   const value = e.target.value;
--- js/index.js
+++ js/index.js
@@ -53,17 +53,9 @@ const prev = document.getElementById("prev-btn");
 
 const carouselWidth = carouselContainer.offsetWidth;
 
-/* Move the carousel 300px backwards 
-change to if carousel is equal to deskpot size use offsetwidth
-and if carousel is equal to mobile use px*/
-
 next.onclick = function () {
   carouselContainer.scrollLeft += carouselWidth;
 };
-
-/* Move the carousel 300px backwards 
-change to if carousel is equal to deskpot size use offsetwidth
-and if carousel is equal to mobile use px*/
 
 prev.onclick = function () {
   carouselContainer.scrollLeft -= carouselWidth;
